dc5596545238b688e08f3538c0a2c9c28c461e49
thuong <thuongthuong051103@gmail.com>
2024-07-26 21:25:12 +0700

    the first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function chooseHDD(imageUrl) {
+    document.getElementById('anhNen').style.backgroundImage = `url('${imageUrl}')`;
+}
+
+function chayDi()
+{
+    const button = document.getElementById('buttonAXT');
+            // Generate random positions
+            const newTop = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+            const newLeft = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+            // Apply new positions
+            button.style.top = newTop + 'px';
+            button.style.left = newLeft + 'px';
+}
+
+document.getElementById('buttonAXT').addEventListener('mouseover', chayDi);
