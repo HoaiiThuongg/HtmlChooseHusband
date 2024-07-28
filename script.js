0a88c920bb53f2b3cb101500f363bda94e7c47bf
thuong <thuongthuong051103@gmail.com>
2024-07-28 21:56:27 +0700

    sua lan 2

--- script.js
+++ script.js
@@ -2,15 +2,45 @@ function chooseHDD(imageUrl) {
     document.getElementById('container').style.backgroundImage = `url('${imageUrl}')`;
 }
 
-function chayDi()
+let currentIndex = 2;
+
+function chooseAXT(imageId)
 {
-    const button = document.getElementById('buttonAXT');
-            // Generate random positions
-            const newTop = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
-            const newLeft = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
-            // Apply new positions
-            button.style.top = newTop + 'px';
-            button.style.left = newLeft + 'px';
+    const img = document.getElementById(imageId);
+
+    img.src = `images/anhTraiXau${currentIndex}.png`;
+
+    // Tăng chỉ số hình ảnh lên 1
+    currentIndex++;
+
+    // Nếu chỉ số hình ảnh vượt quá 4, quay lại 1
+    if (currentIndex > 4) {
+        currentIndex = 1;
+    }
+
+    // Tạo số ngẫu nhiên từ 0 đến 3
+    //const randomIndex = Math.floor(Math.random() * 4);
+
+    // Đổi hình ảnh
+    //img.src = `images/anhTraiXau${randomIndex + 1}.png`;
 }
 
-document.getElementById('buttonAXT').addEventListener('mouseover', chayDi);
+function chayDi()
+{
+    const button = document.getElementById('buttonHDD');
+    button.style.position = 'absolute';
+    // Generate random positions
+    const newTop = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+    const newLeft = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+    // Apply new positions
+    button.style.top = newTop + 'px';
+    button.style.left = newLeft + 'px';
+}
+
+const thienLyOi = new Audio('music/thienLyOi.mp3');
+function playJ97()
+{
+    thienLyOi.play();
+}
+
+document.getElementById('buttonHDD').addEventListener('mouseover', chayDi);
